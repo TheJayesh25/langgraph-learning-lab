bf11dd013c3d1ed3f18755f7298d1eb456a49868
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-05-18 09:19:22 +0530

    Decided on the number of tools and introduced the tool node structure

--- agents/react_style_agent.py
+++ agents/react_style_agent.py
@@ -12,3 +12,18 @@ load_dotenv()
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
+@tool
+def add(a: int, b:int):
+    """This is an addition function that adds 2 numbers together"""
+    return a + b 
+
+@tool
+def subtract(a: int, b: int):
+    """Subtraction function"""
+    return a - b
+
+@tool
+def multiply(a: int, b: int):
+    """Multiplication function"""
+    return a * b
+
