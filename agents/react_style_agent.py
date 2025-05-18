8e5ce197cbc918e4dfbb1f774f723b08ac7350b2
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-05-18 08:50:12 +0530

    Started building tool-enabled agent

---
+++ agents/react_style_agent.py
@@ -0,0 +1,14 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv  
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END, START
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
